af064de6a780db907e957f5bffb2da6062b825d8
dieptonn <diep.bt194505@sis.hust.edu.vn>
2024-04-03 03:05:09 +0700

    add gitignore

--- imgProcessing_opencv/opencv1.py
+++ imgProcessing_opencv/opencv1.py
@@ -1,7 +1,7 @@
 import cv2
 
 img = cv2.imread(
-    'D:\Code\Python\pythonTutorial\R10_PBL\pbl_clone\imgProcessing_opencv\game.png', -1)
+    'D:\Code\Python\pythonTutorial\R10_PBL\AI_reasearch\imgProcessing_opencv\game.png', -1)
 
 # img = cv2.resize(img, (300, 300))  # width=300, height=300
 img = cv2.resize(img, (0, 0), fx=0.6, fy=0.6)
@@ -14,4 +14,4 @@ print(ord('s'))
 
 if (k == ord('s')):
     cv2.imwrite(
-        'D:\Code\Python\pythonTutorial\R10_PBL\pbl_clone\imgProcessing_opencv\gamemoi2.png', img)
+        'D:\Code\Python\pythonTutorial\R10_PBL\AI_reasearch\imgProcessing_opencv\game2.png', img)
